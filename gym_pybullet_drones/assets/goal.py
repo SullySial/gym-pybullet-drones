7780baa19682097a1965ac99e5ccbb238e3517ef
SullySial <suleman.sial@student.uts.edu.au>
2024-05-24 21:15:20 +1000

    Allowed for z value to be changed in urdf

--- gym_pybullet_drones/assets/goal.py
+++ gym_pybullet_drones/assets/goal.py
@@ -6,6 +6,6 @@ class Goal:
     def __init__(self, client, base):
         f_name = os.path.join(os.path.dirname(__file__), 'simplegoal.urdf')
         self.goal = client.loadURDF(fileName=f_name,
-                   basePosition=[base[0], base[1], 0])
+                   basePosition=[base[0], base[1], base[2]])
 
 
